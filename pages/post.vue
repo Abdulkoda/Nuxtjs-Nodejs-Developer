decebfa2a3128effbddd1dc8eb59b5fabb5d6850
koday533 <studenlovely6.ado@gmail.com>
2021-02-22 01:55:51 +0700

    update

--- pages/post.vue
+++ pages/post.vue
@@ -41,18 +41,72 @@
     </b-card>
   </div>
    <div class="mt-5">
-       <b-table striped hover  :items="myProvider" >
-         
+       <b-table striped hover  :items="myProvider" :fields="fields"   disable-pagination :hide-default-footer="true"> 
+        <template #cell(Actions) >
+         <b-button  size="sm" v-b-modal.modal-1 variant="warning" v-model="id" @click="edit(id)">เเก้ไข</b-button>
+
+        <b-button size="sm" v-model="id" @click="deleteTutorial(id)" variant="danger" class="mr-2">
+           ลบ
+        </b-button>
+  
+      </template>
     </b-table>
+     <b-modal id="modal-1" title="เเก้ไข">
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Title:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.Title"
+          type="text"
+          placeholder="Enter Title"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Description:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.Description"
+          placeholder="Enter Description"
+          required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group id="input-group-3" label="Image Url:" label-for="input-2">
+        <b-form-input
+          id="input-3"
+          v-model="form.ImageUrl"
+          placeholder="Enter Image Url"
+          required
+        ></b-form-input>
+      </b-form-group>
+      
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+
+    </b-modal>
   </div>
   </b-container>
 </template>
 
 <script>
-import {HTTP} from "@/axios.js";
+import {http} from "@/http-axios.js";
+import ApiDataService from "../services/ApiDataService.js";
   export default {
     data() {
       return {
+       myProvider: [],
+       fields: [
+          { key: 'id', label: 'ID' },
+          { key: 'Title', label: 'Title' },
+          { key: 'Description', label: 'Description' },
+          { key: 'imageurl', label: 'imageurl' },
+          { key: 'Actions', label: 'actions' }
+        ],
         form: {
           Title: '',
           Description: '',
@@ -62,32 +116,61 @@ import {HTTP} from "@/axios.js";
       }
     },
     methods: {
-     async onSubmit(event){
-          event.preventDefault()
+     async onSubmit(){
+          // event.preventDefault()
           let obj={
             "title":this.form.Title,
             "description":this.form.Description,
             "imageurl":this.form.ImageUrl
           }
-           const result =  await HTTP.post(`/v1/api/post`,obj)
+           const result =  await http.post(`/v1/api/post`,obj)
            console.log(result)
            this.$swal(result.request.response);
         },
-      
-         myProvider () {
-        let promise = HTTP.get('/v1/api/data')
-        console.log(promise);
-        return promise.then((data) => {
-          const items = data.data
-          console.log(items)
-          return(items)
-        }).catch(error => {
-          return []
+    
+       retrieveTutorials() {
+      ApiDataService.getAll()
+        .then((response) => {
+          this.myProvider = response.data.map(this.getDisplayTutorial);
+          console.log(response.data);
         })
-      },
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    refreshList() {
+      this.retrieveTutorials();
+    },
+    getDisplayTutorial(myProvider) {
+      this.id =  myProvider._id
+      return {
+        id: myProvider._id,
+        Title: myProvider.title.length > 30 ? myProvider.title.substr(0, 30) + "..." : myProvider.title,
+        Description: myProvider.description.length > 30 ? myProvider.description.substr(0, 30) + "..." : myProvider.description,
+        imageurl: myProvider.imageurl.length > 30 ? myProvider.imageurl.substr(0, 30) + "..." : myProvider.imageurl,
+      };
+    },
+    deleteTutorial(id) {
+      ApiDataService.delete(id)
+        .then(() => {
+          this.refreshList();
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    edit(id) {
+        ApiDataService.get(id)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
      
      onReset(event) {
-        event.preventDefault()
+        event.preventDefault()  
         // Reset our form values
         this.form.Title = ''
         this.form.Description = ''
@@ -98,6 +181,9 @@ import {HTTP} from "@/axios.js";
           this.show = true
         })
       }
-    }
+    },
+    mounted() {
+    this.retrieveTutorials();
+  },
   }
 </script>
